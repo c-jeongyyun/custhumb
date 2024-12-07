7c17e1e1934992d33a7ae6e8f3b8cca765157b28
c-jeongyyun <99jenny0614@naver.com>
2024-12-07 23:09:34 +0900

    🔥 불필요 헤더 제거

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,5 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API,
   headers: {
     "Access-Control-Allow-Origin": window.location.origin,
-    "Access-Control-Allow-Credentials": "true",
-    withCredentials: "true",
   },
 });
